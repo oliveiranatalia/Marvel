c7dc4bfeb5efa377e907743ebadb4f74390940fc
oliveiranatalia <natalia.oliveira@zup.com.br>
2022-07-26 12:09:52 -0300

    Implementar register

--- app/src/main/java/br/com/zup/marvel/ui/register/view/RegisterActivity.kt
+++ app/src/main/java/br/com/zup/marvel/ui/register/view/RegisterActivity.kt
@@ -1,15 +1,42 @@
 package br.com.zup.marvel.ui.register.view
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.lifecycle.ViewModelProvider
+import br.com.zup.marvel.USER
 import br.com.zup.marvel.databinding.ActivityRegisterBinding
+import br.com.zup.marvel.domain.model.User
+import br.com.zup.marvel.ui.home.view.HomeActivity
+import br.com.zup.marvel.ui.register.viewmodel.RegisterViewModel
+import com.google.android.material.snackbar.Snackbar
 
 class RegisterActivity : AppCompatActivity() {
     private lateinit var binding: ActivityRegisterBinding
+    private val viewModel: RegisterViewModel by lazy { ViewModelProvider(this)[RegisterViewModel::class.java]}
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityRegisterBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        observer()
+        binding.bvBotaoRegistro.setOnClickListener{register()}
+    }
+    private fun observer(){
+        viewModel.register.observe(this){goToHome(it)}
+        viewModel.error.observe(this){ Snackbar.make(binding.root,it, Snackbar.LENGTH_SHORT).show()}
+
+    }    private fun register(){
+        val user = getData()
+        viewModel.validate(user)
+    }
+    private fun getData():User{
+        return User(
+            binding.etEmailRegistro.text.toString(),
+            binding.etSenhaRegistro.text.toString()
+        )
+    }
+    private fun goToHome(user:User){
+        startActivity(Intent(this, HomeActivity::class.java).apply { putExtra(USER,user) })
     }
 }
--- app/src/main/java/br/com/zup/marvel/ui/register/viewmodel/RegisterViewModel.kt
+++ app/src/main/java/br/com/zup/marvel/ui/register/viewmodel/RegisterViewModel.kt
@@ -1,4 +1,35 @@
 package br.com.zup.marvel.ui.register.viewmodel
 
-class RegisterViewModel {
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import br.com.zup.marvel.ERROR
+import br.com.zup.marvel.REQUIRED
+import br.com.zup.marvel.domain.model.User
+import br.com.zup.marvel.domain.repository.AuthenticatonRepository
+
+class RegisterViewModel:ViewModel() {
+    private val repository = AuthenticatonRepository()
+
+    private var _register = MutableLiveData<User>()
+    var register: LiveData<User> = _register
+
+    private var _error = MutableLiveData<String>()
+    var error: LiveData<String> = _error
+
+    fun validate(user:User){
+        when{
+            user.email.isEmpty() -> _error.value = REQUIRED
+            user.password.isEmpty() -> _error.value = REQUIRED
+            else -> register(user)
+        }
+    }
+    private fun register(user:User){
+        try {
+            repository.register(user.email, user.password)
+            _register.value = user
+        }catch(e:Exception){
+            _error.value = ERROR
+        }
+    }
 }
