a7f476e18508379b1de663dc37fb4c90cee0e7f9
oliveiranatalia <natalia.oliveira@zup.com.br>
2022-07-26 20:14:57 -0300

    Update

--- app/src/main/java/br/com/zup/marvel/domain/repository/AuthenticatonRepository.kt
+++ app/src/main/java/br/com/zup/marvel/domain/repository/AuthenticatonRepository.kt
@@ -11,18 +11,17 @@ class AuthenticatonRepository {
 
     private val auth: FirebaseAuth = Firebase.auth
 
-    fun updateUser(name: String): Task<Void>? {
-        val profile = UserProfileChangeRequest.Builder().setDisplayName(name).build()
-        return auth.currentUser?.updateProfile(profile)
-    }
-    fun getUserName(): String = auth.currentUser?.displayName.toString()
-
     fun register(email: String, password: String) : Task<AuthResult> {
         return auth.createUserWithEmailAndPassword(email, password)
     }
     fun login(email: String, password: String) : Task<AuthResult> {
         return auth.signInWithEmailAndPassword(email, password)
     }
+    fun updateUser(name: String): Task<Void>? {
+        val profile = UserProfileChangeRequest.Builder().setDisplayName(name).build()
+        return auth.currentUser?.updateProfile(profile)
+    }
+    fun getUserName(): String = auth.currentUser?.displayName.toString()
     fun logout() {
         auth.signOut()
     }
