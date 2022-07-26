1d992dfd199e3635400e37c55dfa93a9f89a91fb
oliveiranatalia <natalia.oliveira@zup.com.br>
2022-07-26 19:01:34 -0300

    Implementar user name

--- app/src/main/java/br/com/zup/marvel/ui/home/view/HomeActivity.kt
+++ app/src/main/java/br/com/zup/marvel/ui/home/view/HomeActivity.kt
@@ -1,22 +1,26 @@
 package br.com.zup.marvel.ui.home.view
 
+import android.annotation.SuppressLint
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuInflater
+import android.view.MenuItem
 import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.LinearLayoutManager
 import br.com.zup.marvel.*
 import br.com.zup.marvel.databinding.ActivityHomeBinding
 import br.com.zup.marvel.ui.detalhe.DetalheActivity
 import br.com.zup.marvel.data.model.Marvel
+import br.com.zup.marvel.domain.repository.AuthenticatonRepository
 import br.com.zup.marvel.ui.home.viewmodel.HomeViewModel
+import br.com.zup.marvel.ui.login.view.LoginActivity
 
 class HomeActivity : AppCompatActivity() {
     private lateinit var binding: ActivityHomeBinding
-
     private val adapter: MarvelAdapter by lazy {
-        MarvelAdapter(arrayListOf(), this::goToDetail)
-    }
+        MarvelAdapter(arrayListOf(), this::goToDetail)}
 
     private val viewModel: HomeViewModel by lazy {
         ViewModelProvider(this)[HomeViewModel::class.java]
@@ -27,11 +31,17 @@ class HomeActivity : AppCompatActivity() {
         binding = ActivityHomeBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        showUserData()
         viewModel.getListMarvel()
         setUpRecyclerView()
         initObserver()
     }
 
+    private fun showUserData(){
+        val user = viewModel.getUserName()
+        val text = MESSAGE
+        (HI + user + text).also { binding.textView.text = it }
+    }
     private fun setUpRecyclerView() {
         binding.rvHerois.adapter = adapter
         binding.rvHerois.layoutManager = LinearLayoutManager(this)
@@ -49,5 +59,23 @@ class HomeActivity : AppCompatActivity() {
         }
         startActivity(intent)
     }
-
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        val inflater: MenuInflater = menuInflater
+        inflater.inflate(R.menu.menu, menu)
+        return true
+    }
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId) {
+            R.id.exit -> {
+                viewModel.logout()
+                this.finish()
+                goToLogin()
+                true
+            }
+            else -> false
+        }
+    }
+    private fun goToLogin(){
+        startActivity(Intent(this, LoginActivity::class.java))
+    }
 }
--- app/src/main/java/br/com/zup/marvel/ui/home/viewmodel/HomeViewModel.kt
+++ app/src/main/java/br/com/zup/marvel/ui/home/viewmodel/HomeViewModel.kt
@@ -5,9 +5,11 @@ import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import br.com.zup.marvel.data.model.Marvel
 import br.com.zup.marvel.data.repository.MarvelRepository
+import br.com.zup.marvel.domain.repository.AuthenticatonRepository
 
 class HomeViewModel: ViewModel() {
     private val marvelRepository = MarvelRepository()
+    private val repository = AuthenticatonRepository()
 
     private var _marvelListState = MutableLiveData<List<Marvel>>()
     val marvelListState: LiveData<List<Marvel>> = _marvelListState
@@ -16,4 +18,7 @@ class HomeViewModel: ViewModel() {
        val listMarvel =  marvelRepository.getMarvelList()
         _marvelListState.value = listMarvel
     }
+    fun logout() = repository.logout()
+
+    fun getUserName() = repository.getUserName()
 }
