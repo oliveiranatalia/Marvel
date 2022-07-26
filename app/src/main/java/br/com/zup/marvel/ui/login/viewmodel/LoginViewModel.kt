d7a62e58c6e31a5f4e7d68d60449070c861875f5
oliveiranatalia <natalia.oliveira@zup.com.br>
2022-07-26 19:26:29 -0300

    Update

--- app/src/main/java/br/com/zup/marvel/ui/login/viewmodel/LoginViewModel.kt
+++ app/src/main/java/br/com/zup/marvel/ui/login/viewmodel/LoginViewModel.kt
@@ -4,7 +4,7 @@ import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import br.com.zup.marvel.ERROR
-import br.com.zup.marvel.REQUIRED
+import br.com.zup.marvel.LOGIN_ERROR
 import br.com.zup.marvel.domain.model.User
 import br.com.zup.marvel.domain.repository.AuthenticatonRepository
 
@@ -17,8 +17,8 @@ class LoginViewModel: ViewModel() {
 
     fun validate(user:User){
         when{
-            user.email.isEmpty() -> _error.value = REQUIRED
-            user.password.isEmpty() -> _error.value = REQUIRED
+            user.email.isEmpty() -> _error.value = ERROR
+            user.password.isEmpty() -> _error.value = ERROR
             else -> login(user)
         }
     }
@@ -27,7 +27,7 @@ class LoginViewModel: ViewModel() {
         try{
             repository.login(user.email,user.password
             ).addOnSuccessListener { _login.value = user
-            }.addOnFailureListener { _error.value = ERROR + it.message }
-        }catch(e:Exception){_error.value = ERROR}
+            }.addOnFailureListener { _error.value = LOGIN_ERROR + it.message }
+        }catch(e:Exception){_error.value = LOGIN_ERROR}
     }
 }
