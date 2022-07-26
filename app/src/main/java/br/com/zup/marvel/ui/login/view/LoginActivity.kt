f649f3a100ee2ebb57bda0a8c997b58f13f95bd1
oliveiranatalia <natalia.oliveira@zup.com.br>
2022-07-26 12:09:44 -0300

    Implementar login

--- app/src/main/java/br/com/zup/marvel/ui/login/view/LoginActivity.kt
+++ app/src/main/java/br/com/zup/marvel/ui/login/view/LoginActivity.kt
@@ -3,19 +3,44 @@ package br.com.zup.marvel.ui.login.view
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.lifecycle.ViewModelProvider
+import br.com.zup.marvel.USER
 import br.com.zup.marvel.databinding.ActivityLoginBinding
+import br.com.zup.marvel.domain.model.User
+import br.com.zup.marvel.ui.home.view.HomeActivity
+import br.com.zup.marvel.ui.login.viewmodel.LoginViewModel
 import br.com.zup.marvel.ui.register.view.RegisterActivity
+import com.google.android.material.snackbar.Snackbar
 
 class LoginActivity : AppCompatActivity() {
     private lateinit var binding: ActivityLoginBinding
+    private val viewModel: LoginViewModel by lazy { ViewModelProvider(this)[LoginViewModel::class.java] }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityLoginBinding.inflate(layoutInflater)
         setContentView(binding.root)
-
+        observer()
         binding.tvCriarConta.setOnClickListener {
             startActivity(Intent(this, RegisterActivity::class.java))
         }
+        binding.bvBotaoLogin.setOnClickListener{
+            goToLogin()
+        }
+    }
+    private fun observer(){
+        viewModel.login.observe(this){goToHome(it)}
+        viewModel.error.observe(this){ Snackbar.make(binding.root,it, Snackbar.LENGTH_SHORT).show()
+        }
+    }
+    private fun goToLogin(){
+        val user = getData()
+        viewModel.validate(user)
+    }
+    private fun getData(): User{
+        return User(binding.etEmailLogin.text.toString(),binding.etSenhaLogin.text.toString())
+    }
+    private fun goToHome(user:User){
+        startActivity(Intent(this, HomeActivity::class.java).apply { putExtra(USER,user)})
     }
 }
--- app/src/main/java/br/com/zup/marvel/ui/login/viewmodel/LoginViewModel.kt
+++ app/src/main/java/br/com/zup/marvel/ui/login/viewmodel/LoginViewModel.kt
@@ -1,4 +1,33 @@
 package br.com.zup.marvel.ui.login.viewmodel
 
-class LoginViewModel {
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import br.com.zup.marvel.ERROR
+import br.com.zup.marvel.REQUIRED
+import br.com.zup.marvel.domain.model.User
+import br.com.zup.marvel.domain.repository.AuthenticatonRepository
+
+class LoginViewModel: ViewModel() {
+    private val repository = AuthenticatonRepository()
+    private var _login = MutableLiveData<User>()
+    var login: LiveData<User> = _login
+    private var _error = MutableLiveData<String>()
+    var error:LiveData<String> = _error
+
+    fun validate(user:User){
+        when{
+            user.email.isEmpty() -> _error.value = REQUIRED
+            user.password.isEmpty() -> _error.value = REQUIRED
+            else -> login(user)
+        }
+    }
+
+    fun login(user:User){
+        try{
+            repository.login(user.email,user.password
+            ).addOnSuccessListener { _login.value = user
+            }.addOnFailureListener { _error.value = ERROR + it.message }
+        }catch(e:Exception){_error.value = ERROR}
+    }
 }
